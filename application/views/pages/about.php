f88639c818497b4dce85c7322a15033f10946eb8
Roberto Arciniegas <rarciniegas@hotmail.com>
2019-12-04 03:39:16 -0500

    Home and About modified

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,7 +1,19 @@
-  <div class="jumbotron">
+<!--
+ <div class="jumbotron">
     <div class="container">
       <h1 class="display-3">About</h1>
       <p>Miles log keeps track of your vehicle events. Accurate mileage reports. Stress-free logging. On your desktop. In the palm of your hand.</p>
       <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
     </div>
   </div>
+-->
+<div class="col-md-12 mx-auto">
+<div class="card" >
+  <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/causeway.jpg" alt="Card image">
+  <div class="card-img-overlay">
+    <h1 class="card-title">About</h1>
+    <p class="card-text">Miles log keeps track of your vehicle events. Accurate mileage reports. Stress-free logging. On your desktop. In the palm of your hand.</p>
+    <a href="#" class="btn btn-primary">Learn more</a>
+  </div>
+</div>
+</div>
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,8 +1,18 @@
-
+<!--
     <div class="jumbotron">
       <h1 class="display-3">Home</h1>
       <p>Miles log tracks your mileage automatically using GPS so you no longer need to keep a mileage log or logbook -- all you need to do is let the app record your trips in the background and swipe trips to the left if personal or to the right if for business.</p>
+
+    </div>
+      -->
+      <div class="col-md-12 mx-auto">
+<div class="card" >
+  <img class="card-img-top" src="<?php echo base_url(); ?>assets/images/causeway.jpg" alt="Card image">
+  <div class="card-img-overlay">
+    <h1 class="card-title">Home</h1>
+    <p class="card-text">Miles log tracks your mileage automatically using GPS so you no longer need to keep a mileage log or logbook -- all you need to do is let the app record your trips in the background and swipe trips to the left if personal or to the right if for business.</p>
       <?php if($this->session->userdata('logged_in')) : ?>
       	<p><a class="btn btn-primary btn-lg" href="<?php echo base_url(); ?>vehicles/fuelup" role="button">Fuel up &raquo;</a></p>
-      <?php endif;?>
-    </div>
+      <?php endif;?>  </div>
+</div>
+</div>
